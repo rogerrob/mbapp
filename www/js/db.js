63293b226e4399ca199c6f6f8cc5b281c5dd997a
Ro66ie <rogerro66ie@hotmail.co.uk>
2018-04-27 13:34:09 +0100

    fixed indexeddb, working

--- www/js/db.js
+++ www/js/db.js
@@ -1,7 +1,7 @@
 //creating initial database
 var db;
 var databaseName = 'dbdiet';
-var databaseVersion = 1;
+var databaseVersion = 2;
 var openRequest = indexedDB.open(databaseName, databaseVersion);
 openRequest.onupgradeneeded = function (event) {
     // This is either a newly created database, or a new version number
@@ -9,7 +9,7 @@ openRequest.onupgradeneeded = function (event) {
     var db = event.target.result;
 
     if(!db.objectStoreNames.contains('tableentry')){
-      var os = db.createObjectStore('tableentry', { keyPath: 'timedate' });
+      var os = db.createObjectStore('tableentry', { keyPath: 'timedate'});
       //Create Index for Name
       os.createIndex('activity','activity',{unique:false});
 
@@ -23,7 +23,7 @@ openRequest.onupgradeneeded = function (event) {
     // the key is stored. If a key path is specified, the store can only contain
     // JavaScript objects, and each object stored must have a property with the
     // same name as the key path (unless the autoIncrement option is true).
-    var store = db.createObjectStore('tableentry', { keyPath: 'timedate' });
+    var store = db.createObjectStore('tableentry', { keyPath: 'timedate'});
 
 //problems with the storing name if not unique from previous
 
@@ -34,18 +34,19 @@ openRequest.onupgradeneeded = function (event) {
     // syntax: store.createIndex(indexName, keyPath[, parameters]);
     //
     // All these values could have duplicates, so set unique to false
-    //store.createIndex('cheese', 'cheese', { unique: false });
+    //store.createIndex('id', 'id', { unique: true });
     store.createIndex('activity', 'activity', { unique: false });
     store.createIndex('calories', 'calories', { unique: false });
     store.createIndex('meal', 'meal', { unique: false });
     store.createIndex('notes', 'notes', { unique: false });
-    store.createIndex('timedate', 'timedate', {unique: true});
+    store.createIndex('timedate', 'timedate', {unique: false});
 
     // Once the store is created, populate it
     store.transaction.oncomplete = function(event) {
       // Store values in the newly created objectStore.
       var ObjectStore = db.transaction("tableentry", "readwrite").objectStore("tableentry");
-      store.forEach(function(tableentrys) {
+      // change the XXX.foreach name if DB = "name exists"
+      change.forEach(function(tableentrys) {
         ObjectStore.add(tableentrys);
       });
     };
@@ -60,7 +61,6 @@ openRequest.onerror = function (event) {
 };
 
 function addData() {
-  var tableentrysid = ''
   var activity = $$('#activity').val();
   var meal = $$('#meal').val();
   var calories = $$('#calories').val();
@@ -131,7 +131,7 @@ function displayData(event) {
 	index.openCursor().onsuccess = function(event){
 		var cursor = event.target.result;
 		if(cursor){
-      output += "<tr ide='newItem_"+cursor.value.activity+"'>";
+      output += "<tr id='newItem_"+cursor.value.activity+"'>";
       output += "<td<span class='cursor newItem'>">+cursor.value.activity+"</span></td>";
 			output += "<td><span class='cursor newItem'>"+cursor.value.meal+"</span></td>";
       output += "<td><span class='cursor newItem'>"+cursor.value.calories+"</span></td>";
@@ -145,3 +145,9 @@ function displayData(event) {
 	};
 };
 //https://www.eduonix.com/dashboard/Learn-HTML5-IndexedDB-App
+
+function deleteDB(){
+  indexedDB.deleteDatabase(databaseName);
+  alert("Database deleted, please reload program");
+  window.location.href="index.html";
+}
